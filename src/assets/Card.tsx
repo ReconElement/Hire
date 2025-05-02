c65b13530bf33fefb5dc6a2ebcc9d7f633516b2b
ReconElement <132275628+ReconElement@users.noreply.github.com>
2025-05-02 19:13:53 +0530

    Added some code
    Added some code

--- src/assets/Card.tsx
+++ src/assets/Card.tsx
@@ -1,18 +1,18 @@
 import '../App.css';
-const Card = ()=>{
+const Card = (props: {title: string, content: string, buttonContent: string, func: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>void})=>{
     return(
-        <div className="relative flex flex-col my-6 bg-gradient-to-r from-lightviolet to-darkviolet text-gray-400 shadow-sm border border-slate-200 border-transparent rounded-lg w-145">
+        <div className="relative flex flex-col my-6 bg-gradient-to-r from-lightviolet to-darkviolet text-gray-400 shadow-sm border border-transparent shadow-emerald-800 rounded-lg w-145">
             <div className="p-4">
                 <div className="mb-2 text-slate-200 text-2xl poppins-regular poppins-semibold ">
                     <span className="">
-                        Practice customised mock interview with AI!
+                        {props.title}
                     </span>
                 </div>
                 <div className="text-slate-200 leading-normal text-xl poppins-light">
-                    Your results will be visible only to you
+                    {props.content}
                 </div>
-                <button type="button" className="rounded-md poppins-light bg-darkviolet py-2 px-4 mt-6 border border-transparent text-center text-base text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-300">
-                    Register now!
+                <button onClick={props.func} type="button" className="rounded-md poppins-light bg-darkviolet py-2 px-4 mt-6 border border-transparent text-center text-base text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-300 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none ">
+                    {props.buttonContent}
                 </button>
             </div>
         </div>
--- src/components/Landing/Content.tsx
+++ src/components/Landing/Content.tsx
@@ -1,6 +1,13 @@
 import '../../App.css';
 import Card from '../../assets/Card';
+import { useNavigate } from 'react-router';
 const Content = ()=>{
+    const navigate = useNavigate();
+    const signup = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        navigate('/signup');
+    }
     return (
         <div className="py-18 px-3 ">
             <div>
@@ -10,21 +17,7 @@ const Content = ()=>{
                 <span className="flex justify-center poppins-light text-2xl">More than 5 lac+ jobs for you to find</span>
             </div>
             <div>
-                {/* refer to https://www.material-tailwind.com/docs/html/card#blog-card for these tailwind styles */}
-                {/* <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
-                    <div className="p-4">
-                        <h5 className="mb-2 text-slate-800 text-xl font-semibold">
-                            Practice customised mock interview with AI!
-                        </h5>
-                        <p className="text-slate-600 leading-normal font-light">
-                            Don't worry your result will be visible only to you
-                        </p>
-                        <button type="button" className="rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700  hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                            Sign up now! 
-                        </button>
-                    </div>
-                </div> */}
-                <Card/>
+                <Card title="Practice customised mock interview with AI!" content="Your result will be visible only to you" buttonContent="Sign up now!" func={signup} />
             </div>
         </div>
     )
