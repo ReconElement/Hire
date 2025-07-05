7b55d2457052a9e34d21b30e9076d06c3f505299
ReconElement <132275628+ReconElement@users.noreply.github.com>
2025-07-05 22:27:54 +0530

    Added some code
    Added some code

--- src/components/Posts/PostDetail.tsx
+++ src/components/Posts/PostDetail.tsx
@@ -1,5 +1,6 @@
 import '../../App.css';
 import { useState } from 'react';
+import {create} from 'zustand';
 type Post = {
     id: number,
     title: string,
@@ -8,7 +9,32 @@ type Post = {
     updatedAt: Date | null,
     authorId: number
 }
+interface Content{
+    data: {
+        title?: string,
+        content?: string
+    }
+    updateTitle?: (e: React.ChangeEvent<HTMLInputElement>)=>void,
+    updateContent?: (e: React.ChangeEvent<HTMLTextAreaElement>)=>void,
+}
+const useContents = create<Content>()((set)=>({
+    data: {
+        title: undefined,
+        content: undefined,
+    },
+    updateTitle: (e: React.ChangeEvent<HTMLInputElement>)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        set(()=>({data: {title: e.target.value}}));
+    },
+    updateContent: (e: React.ChangeEvent<HTMLTextAreaElement>)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        set(()=>({data: {content: e.target.value}}));
+    }
+}));
 const PostDetail = ({id,post}:{id: number, post: Post})=>{
+    const {data, updateTitle, updateContent} = useContents();
     const [editFlag, setEditFlag] = useState(false);
     const postId = id.toString();
     const authId: string | null = localStorage.getItem('id');
@@ -40,14 +66,14 @@ const PostDetail = ({id,post}:{id: number, post: Post})=>{
                     {match && <button type="button" onClick={onClick} className="m-2 p-2 shadow-sm shadow-lightblue rounded-md active:shadow-none focus:shadow-sm" >Edit</button>}
                 </div>
             </div>
-            {match && editFlag?<div><Edit id={post.id} post={post} /></div>:<div><Display id={post.id} post={post}/></div>}
+            {match && editFlag?<div><Edit id={post.id} post={post} /></div>:<div></div>}
         </div> 
     )
 };
 
 const Display =({id, post}:{id: number, post: Post})=>{
     return(
-        <div id={id.toString()} className="bg-darkviolet poppins-regular">
+        <div id={id.toString()} className="bg-darkviolet poppins-regular p-2 m-2">
             <div>
                 {post.title}
             </div>
@@ -57,31 +83,49 @@ const Display =({id, post}:{id: number, post: Post})=>{
         </div>
     )
 }
-const Edit =({id, post}:{id: number, post: Post})=>{
-    const [value, setValue] = useState(post?.content || '');
-    const onSubmit = function(e: React.FormEvent<HTMLInputElement>){
+// const Edit =({id, post}:{id: number, post: Post})=>{
+//     const [value, setValue] = useState(post?.content || '');
+//     const onSubmit = function(e: React.FormEvent<HTMLInputElement>){
+//         e.preventDefault();
+//         e.stopPropagation();
+//         console.log(value);
+//     }
+//     const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>)=>{
+//         e.preventDefault();
+//         e.stopPropagation();
+//         setValue(e.target.value);
+//     }
+//     return(
+//         <div id={id.toString()} className="bg-darkviolet poppins-regular">
+//             <div className="m-2 p-2">
+//                 <input type="text" title="title" name="title" id="title" value={post.title} />
+//             </div>
+//             <div>
+//                 <form action="">
+//                     <textarea id="content" title="content" name="content" className="" value={value} onChange={onChange}></textarea>
+//                     <input type="submit" value="Submit" onClick={onSubmit} />
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
+
+const Edit = ({id, post, data, updateTitle, updateContent}:{id: number, post: Post, data: Content["data"], updateTitle: Content["updateTitle"],updateContent: Content["updateContent"] })=>{
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
         e.stopPropagation();
-        console.log(value);
-    }
-    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        setValue(e.target.value);
+        console.log(data.title);
+        console.log(data.content);
     }
     return(
-        <div id={id.toString()} className="bg-darkviolet poppins-regular">
-            <div>
-                <input type="text" title="title" name="title" id="title" value={post.title} />
-            </div>
-            <div>
-                <form action="" onSubmit={(e)=>{}}>
-                    <textarea id="content" title="content" name="content" className="" value={value} onChange={onChange}></textarea>
-                    <input type="submit" value="" onClick={(e)} />
-                </form>
-            </div>
+        <div className="p-2 m-2 poppins-regular">
+            <form action="" onSubmit={onSubmit}>
+                <input type="text" name="title" id="title" value={data.title} placeholder={post.title} onChange={updateTitle}/>
+                <textarea name="content" value={data.content} id="content" placeholder={post.content} onChange={updateContent}></textarea>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
-}
+};
 
 export default PostDetail;
